c4026d8a201418b9876184132a6d7c0b926a2eb1
Alamek <162360463+AO247@users.noreply.github.com>
2024-11-23 07:18:01 +0100

    smallfix

--- Assets/Scripts/WeaponPlayer.cs
+++ Assets/Scripts/WeaponPlayer.cs
@@ -5,8 +5,14 @@ public class WeaponPlayer : MonoBehaviour
 {
     [SerializeField] float damage;
     [SerializeField] float delay;
+    [SerializeField] float beforeAttackTime;
+    [SerializeField] float afterAttackTime;
     [SerializeField] float knockback;
-    bool IsAttacking = false;
+    bool isAttacking = false;
+    bool attackStop = false;
+
+    float baTime = 0f;
+    float aaTime = 0f;
     float delayTime = 0f;
     List<Collider2D> _enemies = new List<Collider2D>();    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,27 +21,48 @@ public class WeaponPlayer : MonoBehaviour
     }
     public void Attack()
     {
-        if (delayTime <= 0)
+        if (delayTime <= 0 && !attackStop)
         {
-            delayTime = delay;
-            IsAttacking = true;
+            attackStop = true;
+            baTime = beforeAttackTime;
         }
 
     }
     // Update is called once per frame
     void Update()
     {
+        if (baTime > 0)
+        {
+            baTime -= Time.deltaTime;
+        }
+        else if (baTime < 0 && attackStop == true)
+        {
+            isAttacking = true;
+            aaTime = afterAttackTime;
+            baTime = 0;
+        }
+
+        if (aaTime > 0)
+        {
+            aaTime -= Time.deltaTime;
+        }
+        else if (aaTime <= 0 && baTime == 0 && attackStop == true)
+        {
+            delayTime = delay;
+            attackStop = false;
+        }
+
         if (delayTime > 0)
         {
             delayTime -= Time.deltaTime;
         }
-        if (IsAttacking)
+        if (isAttacking)
         {
             for (int i = 0; i < _enemies.Count; i++)
             {
                 _enemies[i].GetComponent<HealthEnemy>().GetHit(damage);
             }
-            IsAttacking = false;
+            isAttacking = false;
 
         }
     }
